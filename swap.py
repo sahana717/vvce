2f7013b2733840ead5b688a5cc41bf69c093435a
sahana717 <vvce22ise0026@vvce.ac.in>
2025-03-27 11:01:07 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,12 @@
+# Initialize numbers
+a = 5
+b = 10
+
+print("Before swapping: a =", a, ", b =", b)
+
+# Swapping using a temporary variable
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, ", b =", b)
